5e9ceef2c9bd54c7cfc54b6cb014783ba25b0a99
Mustafa-Seif <mustafaabdella64@gmail.com>
2022-11-14 16:05:20 +0200

    fix style

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import aboutIMG      from '../../assets/5078a05eb1b6847d93383eaa4c0ed500.gif'
 
-function About() {
+const About = () => {
   return (
-    <div>About</div>
+   
+      <div className='container mt-5'>
+      <div className='row text-center mb-5'>
+      <h1>About Us</h1>
+      <hr/>
+    </div>
+        <div className='row'>
+          <div className='col-6'>
+            <h1>About Us</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Voluptates minus optio ipsa expedita deleniti pariatur amet 
+            voluptatibus quidem cumque veniam recusandae, dolores soluta!
+             Quo incidunt laudantium perspiciatis error nesciunt distinctio
+              nemo velit a, aut sapiente, neque rerum at libero expedita doloribus
+               accusantium consectetur. Ullam sunt nulla accusamus aliquam, aut
+                tempore facere dolore dicta ex labore impedit praesentium fuga, 
+                cum distinctio dolor, neque adipisci. Corporis assumenda quaerat 
+                nobis animi ipsum cupiditate quibusdam modi similique veniam nisi 
+                impedit mollitia exercitationem explicabo itaque voluptas, quia iusto 
+                reprehenderit vero culpa saepe. Error animi beatae ut facere optio, ea 
+                commodi odit natus qui, ducimus asperiores.</p>
+                <NavLink to='/contact' className='btn btn-outline-primary'>Contact Us</NavLink>
+          </div>
+          <div className='col-6'>
+          <img src={aboutIMG} alt="..." width='100%'/>
+        </div>
+        </div>
+        
+      </div>
   )
 }
 
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import aboutIMG      from '../../assets/contact-us-customer-service-illustration_2175-310.webp'
 
-function Contact() {
+
+const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='container mt-5'>
+    <div className='row text-center mb-5'>
+      <h1>Contact With Us</h1>
+      <hr/>
+    </div>
+    <div className='row'>
+      <form style={{width:'50%'}} className='col-6'>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+      <div className='col-6'>
+        <img src={aboutIMG} alt="" width='100%' />
+      </div>
+      </div>
+    </div>
   )
 }
 
